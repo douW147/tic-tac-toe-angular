6beaf5a39edbb733aea4c6e10d645b80be829cce
Olexandr Stepanenko <sashok1111122222@gmail.com>
2022-08-15 20:35:36 +0300

    add: express server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/tic-tac-toe-angular'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/tic-tac-toe-angular/index.html'));});
+app.listen(process.env.PORT || 8080);
